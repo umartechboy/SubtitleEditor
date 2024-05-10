db99f3262cd3c761038d5a68e5f47cf44e79a996
Muhammad Umar Hassan <umar.techboy@gmail.com>
2024-05-10 20:30:16 +0500

    Testing with web server

---
+++ ServerSide/Data/WeatherForecastService.cs
@@ -0,0 +1,28 @@
+namespace ServerSide.Data
+{
+	public class WeatherForecastService
+	{
+		private static readonly string[] Summaries = new[]
+		{
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
+
+		public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+		{
+			var mre = new ManualResetEvent(false);
+			new Thread(() =>
+			{
+				Thread.Sleep(1000);
+				mre.Set();
+			}).Start();
+			mre.WaitOne();
+			Console.WriteLine("GetForecastAsync() being called");
+			return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			{
+				Date = startDate.AddDays(index),
+				TemperatureC = Random.Shared.Next(-20, 55),
+				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+			}).ToArray());
+		}
+	}
+}
